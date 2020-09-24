2d69e6924f369906723a2f10cfbdcf9ace453062
suzukishoei <suzuki.shoei@gmail.com>
2020-09-24 11:00:33 +0900

    kakusu

---
+++ www/js/env.js
@@ -0,0 +1,3 @@
+var refresh_token = '1//0eVuEl9SFq3yGCgYIARAAGA4SNwF-L9IrXwjfd0FvFUy_m8i4m_MUNNMddRGHY8mqY8-RwK4sS2Bw4a4pHHA7f7PLz5jKa9OJBKw'  
+var client_id = '437581889525-s45ra27kiu565qn3oot7h28vp1qfkq51.apps.googleusercontent.com'  
+var client_secret = '82wcIj_2acBjBsnyLIPPn9Cp'  
